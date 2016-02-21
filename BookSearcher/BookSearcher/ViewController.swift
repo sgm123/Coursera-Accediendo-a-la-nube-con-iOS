95b650164bbb6c506ec71adf0ec843b7111d3702
Sergio García <sergiogm.amurrio@gmail.com>
2016-02-21 23:25:24 +0100

    json parse

--- BookSearcher/BookSearcher/ViewController.swift
+++ BookSearcher/BookSearcher/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var inputTextField: UITextField!
-    @IBOutlet weak var resultTextView: UITextView!
+//    @IBOutlet weak var resultTextView: UITextView!
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var authors = [String]() {
+        didSet {
+            if let tableView = self.tableView {
+                tableView.reloadData()
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         inputTextField.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.allowsSelection = false
     }
     
     func downloadBookDataWith(isbn isbn: String) {
@@ -24,7 +37,7 @@ class ViewController: UIViewController {
             let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
                 guard data != nil && error == nil else {
-                    self.updateResultTextView("")
+                    self.updateResultTextView("", isbn: nil)
                     dispatch_async(dispatch_get_main_queue()) { () -> Void in
                         let alert = UIAlertController(title: "Error conexión", message: "No hay conexión a internet o el servidor no está accesible.", preferredStyle: .Alert)
                         alert.addAction(UIAlertAction(title: "Aceptar", style: .Default, handler: nil))
@@ -34,23 +47,73 @@ class ViewController: UIViewController {
                 }
                 let json = String(data: data!, encoding: NSUTF8StringEncoding)
                 if let json = json {
-                    self.updateResultTextView(json)
+                    self.updateResultTextView(json, isbn: isbn)
                 } else {
-                    self.updateResultTextView("Ha ocurrido un error")
+                    self.updateResultTextView("Ha ocurrido un error", isbn: nil)
                 }
             })
             task.resume()
             UIApplication.sharedApplication().networkActivityIndicatorVisible = true
         } else {
             // Error building endpoint
-            self.resultTextView.text = "Ha ocurrido un error generando la url \n \(urlString)"
+             print("Ha ocurrido un error generando la url \n \(urlString)")
+            //self.resultTextView.text = "Ha ocurrido un error generando la url \n \(urlString)"
         }
     }
     
-    func updateResultTextView(text: String) {
+    func updateResultTextView(text: String, isbn: String?) {
         dispatch_async(dispatch_get_main_queue()) { () -> Void in
-            self.resultTextView.text = text
+            print(text)
+            let json = self.convertStringToDictionary(text)
+            if let json = json, isbn = isbn {
+                let stringArray = isbn.componentsSeparatedByCharactersInSet(
+                    NSCharacterSet.decimalDigitCharacterSet().invertedSet)
+                let newString = stringArray.joinWithSeparator("")
+                let coverURL = "http://covers.openlibrary.org/b/isbn/\(newString)-L.jpg"
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), { () -> Void in
+                    // do some task
+                    let url = NSURL(string: coverURL)
+                    if let url = url {
+                        if let data = NSData(contentsOfURL: url) {
+                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                                self.mainImageView.image = UIImage(data: data)
+                            })
+                        } else {
+                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                                self.mainImageView.image = nil
+                            })
+                        }
+                    }
+                })
+                let bookTitle = json["ISBN:\(isbn)"]?["title"] as? String
+                let bookAuthors = json["ISBN:\(isbn)"]?["authors"] as? [[String: String]]
+                if let bookAuthors = bookAuthors {
+                    var names = [String]()
+                    for author in bookAuthors {
+                        names.append(author["name"] ?? "")
+                    }
+                    self.authors = names
+                } else {
+                    self.authors = [String]()
+                }
+                self.titleLabel.text = bookTitle
+            } else {
+                self.authors = [String]()
+                // Borrar datos
+            }
+            //self.resultTextView.text = text
         }
+    }
+    
+    private func convertStringToDictionary(text: String) -> [String:AnyObject]? {
+        if let data = text.dataUsingEncoding(NSUTF8StringEncoding) {
+            do {
+                return try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String: AnyObject]
+            } catch let error as NSError {
+                print(error)
+            }
+        }
+        return nil
     }
     
 }
@@ -62,6 +125,22 @@ extension ViewController: UITextFieldDelegate {
         }
         textField.resignFirstResponder()
         return true
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Autores"
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return authors.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: "default")
+        cell.textLabel?.text = authors[indexPath.row]
+        return cell
     }
 }
 
